dd72d4c66e1b416062fe4d97b136bb83c2b590b9
Dipti Sharma <sharmadipti546@gmail.com>
2024-02-04 00:58:07 +0530

    till checkoutpage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,30 @@ import React from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignPage></SignPage>,
+  },
+]);
 
 function App() {
   return (
@@ -55,8 +79,8 @@ function App() {
       
     // </div>
     <div className='App'>
-     <Home></Home>
-    </div>
+     <RouterProvider router={router} />
+    </div> 
 
   );
 }
